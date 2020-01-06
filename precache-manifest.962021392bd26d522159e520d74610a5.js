bff6d61d2284b812be79998fd5b84f12af8d16ad
Ilan Amzallag <Ilan@air-de-amzallag.lan>
2020-01-06 04:13:35 +0100

    Updates

---
+++ precache-manifest.962021392bd26d522159e520d74610a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ed3aa48ee7f541fd5e47b55b414e7f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c4fb186f46dc4e7a8fb9",
+    "url": "/todolist/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c4fb186f46dc4e7a8fb9",
+    "url": "/todolist/static/js/2.8c63ad5f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.8c63ad5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "243212e1e34ce9e8f127",
+    "url": "/todolist/static/js/main.9f392299.chunk.js"
+  },
+  {
+    "revision": "507716673e279f58353e",
+    "url": "/todolist/static/js/runtime-main.948c6c71.js"
+  }
+]);
